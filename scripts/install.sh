1e42b63644a30394e571427da2b2905a49898c38
minhanghuang <job@minhang.me>
2023-02-05 14:58:12 +0800

    chore: update script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
 set -e
 
-current_path=$(cd $(dirname $0) && pwd)
-install_prefix="$current_path/../install/"
+CURRENT_PATH=$(cd $(dirname $0) && pwd)
+INSTALL_PREFIX="$CURRENT_PATH/../install/"
 ARCH=$(uname -m)
 
 function download() {
   URL=$1
   LIB_NAME=$2
-  DOWNLOAD_PATH="$current_path/../third_party/$LIB_NAME/"
+  DOWNLOAD_PATH="$CURRENT_PATH/../third_party/$LIB_NAME/"
   if [ -e $DOWNLOAD_PATH ]
   then
     echo ""
@@ -24,39 +24,44 @@ function build_setup() {
   echo "############### Build Setup. ################"
   local NAME="setup"
   download "https://github.com/minhanghuang/setup.git" "$NAME"
-  pushd "$current_path/../third_party/$NAME/"
+  pushd "$CURRENT_PATH/../third_party/$NAME/"
   mkdir -p build && cd build
-  cmake -DCMAKE_INSTALL_PREFIX=$install_prefix -DBUILD_SHARED_LIBS=ON ..
+  cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON ..
   make install
   popd
 }
 
-# function build_tinyxml2() {
-#   echo "############### Build Tinyxml2. ################"
-#   local NAME="tinyxml2"
-#   download "https://github.com/leethomason/tinyxml2.git" "$NAME"
-#   pushd "$current_path/../third_party/$NAME/"
-#   git checkout 9.0.0
-#   mkdir -p build && cd build
-#   cmake -DCMAKE_INSTALL_PREFIX=$install_prefix ..
-#   make -j8
-#   make install
-#   popd
-# }
-
 function build_fastdds() {
   echo "############### Build Fast-DDS. ################"
-  download "https://github.com/eProsima/Fast-RTPS.git" "Fast-RTPS"
-  pushd "$current_path/../third_party/Fast-RTPS/"
-  git checkout v1.5.0
-  git submodule init
-  git submodule update
-  # patch -p1 < "$current_path/../scripts/FastRTPS_1.5.0.patch"
-  mkdir -p build && cd build
-  cmake -DEPROSIMA_BUILD=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$install_prefix ..
-  make -j$(nproc)
-  make install
-  popd
+  # download "https://github.com/eProsima/Fast-RTPS.git" "Fast-RTPS"
+  # pushd "$CURRENT_PATH/../third_party/Fast-RTPS/"
+  # git checkout v1.5.0
+  # git submodule init
+  # git submodule update
+  # patch -p1 < "$CURRENT_PATH/../scripts/FastRTPS_1.5.0.patch"
+  # mkdir -p build && cd build
+  # cmake -DEPROSIMA_BUILD=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX ..
+  # make -j$(nproc)
+  # make install
+  # popd
+  
+  local INSTALL_PATH="$CURRENT_PATH/../third_party/"
+  if [[ "${ARCH}" == "x86_64" ]]; then
+    PKG_NAME="fast-rtps-1.5.0-1.prebuilt.x86_64.tar.gz"
+  else # aarch64
+    PKG_NAME="fast-rtps-1.5.0-1.prebuilt.aarch64.tar.gz"
+  fi
+  DOWNLOAD_LINK="https://apollo-system.cdn.bcebos.com/archive/6.0/${PKG_NAME}"
+  if [ -e $INSTALL_PATH/$PKG_NAME ]
+  then
+    echo ""
+  else
+    wget $DOWNLOAD_LINK -P $INSTALL_PATH
+  fi
+  pushd $INSTALL_PATH
+  tar -zxf ${PKG_NAME}
+  cp -r fast-rtps-1.5.0-1/* ../install
+  rm -rf fast-rtps-1.5.0-1
 }
 
 function build_gfamily() {
@@ -67,54 +72,53 @@ function build_gfamily() {
   download "https://github.com/protocolbuffers/protobuf.git" "protobuf"
 
   # gflags
-  pushd "$current_path/../third_party/gflags/"
+  pushd "$CURRENT_PATH/../third_party/gflags/"
   git checkout v2.2.0
   mkdir -p build && cd build
-  CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$install_prefix -DBUILD_SHARED_LIBS=ON ..
+  CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON ..
   make -j$(nproc)
-  make install
+  sudo make install
   popd
 
   # glog
-  pushd "$current_path/../third_party/glog/"
+  pushd "$CURRENT_PATH/../third_party/glog/"
   git checkout v0.3.5
   mkdir -p build && cd build
   if [ "$ARCH" == "x86_64" ]; then
-    CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$install_prefix -DBUILD_SHARED_LIBS=ON ..
+    CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON ..
   elif [ "$ARCH" == "aarch64" ]; then
-    CXXFLAGS="-fPIC" cmake --build=armv8-none-linux -DCMAKE_INSTALL_PREFIX=$install_prefix -DBUILD_SHARED_LIBS=ON ..
+    CXXFLAGS="-fPIC" cmake --build=armv8-none-linux -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON ..
   else
       echo "not support $ARCH"
   fi
   make -j$(nproc)
-  make install
+  sudo make install
   popd
   
   # googletest
-  pushd "$current_path/../third_party/googletest/"
+  pushd "$CURRENT_PATH/../third_party/googletest/"
   git checkout release-1.10.0
   mkdir -p build && cd build
-  CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$install_prefix -DBUILD_SHARED_LIBS=ON ..
+  CXXFLAGS="-fPIC" cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON ..
   make -j8
-  make install
+  sudo make install
   popd
 
   # protobuf
-  pushd "$current_path/../third_party/protobuf/"
+  pushd "$CURRENT_PATH/../third_party/protobuf/"
   git checkout v3.14.0
   ./autogen.sh
-  ./configure --prefix=$install_prefix --enable-shared
+  ./configure --prefix=$INSTALL_PREFIX --enable-shared
   make -j$(nproc)
-  make install
+  sudo make install
   popd
 }
 
 function main() {
   echo "############### Install Third Party. ################"
   build_setup
-  # build_tinyxml2
+  build_gfamily
   build_fastdds
-  # build_gfamily
   return
 }
 
