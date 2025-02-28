79d990fa969d46e96c191912d3dfd114ce217155
minhanghuang <job@minhang.me>
2025-02-28 12:02:50 +0800

    style(script): code format

--- install.py
+++ install.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-import subprocess
-import re
-import os
+# coding:utf-8
+
 import argparse
-import time
+import os
 import platform
+import re
+import subprocess
+
 
 class Install:
-
     def __init__(self, platform, install_prefix) -> None:
         self._machine = platform
         self._home_path = os.path.expanduser("~")
         self._current_path = os.path.abspath(os.path.dirname(__file__))
         self._dowload_path = os.path.join(self._current_path, "third_party")
         self._install_prefix = os.path.join(self._current_path, "install")
-        if (install_prefix != "install"):
+        if install_prefix != "install":
             self._install_prefix = install_prefix
 
         if not os.path.exists(self._install_prefix):
@@ -23,13 +24,12 @@ class Install:
         self.environment = self._load_environment()
 
     def _load_environment(self):
-        '''
+        """
             In case of cross-compilation, we need to load the environment setup file
         :return: env
-        '''
+        """
         env = os.environ.copy()
         return env
-
 
     def _cmd(self, command):
         """执行系统命令，使用指定的环境变量"""
@@ -46,20 +46,17 @@ class Install:
         # # self._clone_dds()
         self._clone_dds2()
         self._clone_nlohmann_json()
-        self._clone_proj() # ros_bridge of apollo v10
+        self._clone_proj()  # ros_bridge of apollo v10
         self._clone_gfamily()
-        self._clone_gperftools() # apollo v10
-        self._unpack_bvar() # apollo v10
+        self._clone_gperftools()  # apollo v10
+        self._unpack_bvar()  # apollo v10
 
     def _clone_github_repo(self, repo_url, repo_name, *args):
         dowload_path = os.path.join(self._dowload_path, repo_name)
         if os.path.exists(dowload_path):
             print("repo exists: {}".format(dowload_path))
             return None
-        command = "git clone {} {}".format(
-            repo_url,
-            dowload_path
-        )
+        command = "git clone {} {}".format(repo_url, dowload_path)
         for arg in args:
             command += " " + arg
         print("clone: {}".format(command))
@@ -70,15 +67,12 @@ class Install:
 
     def _clone_setup(self):
         self._clone_github_repo(
-            "https://github.com/minhanghuang/setup.git",
-            "setup",
-            "--depth=1"
+            "https://github.com/minhanghuang/setup.git", "setup", "--depth=1"
         )
         os.chdir(os.path.join(self._dowload_path, "setup"))
         self._cmd("mkdir -p build")
         os.chdir("build")
-        self._cmd(
-            "cmake -DCMAKE_INSTALL_PREFIX={} ..".format(self._install_prefix))
+        self._cmd("cmake -DCMAKE_INSTALL_PREFIX={} ..".format(self._install_prefix))
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -86,14 +80,21 @@ class Install:
         print("start to install gcc")
         gcc_version = "0.0"
         try:
-            result = subprocess.run(['gcc', '--version'], stdout=subprocess.PIPE, universal_newlines=True, check=True)
+            result = subprocess.run(
+                ["gcc", "--version"],
+                stdout=subprocess.PIPE,
+                universal_newlines=True,
+                check=True,
+            )
             output = result.stdout.splitlines()[0]
-            match = re.search(r'(\d+\.\d+\.\d+)', output)
+            match = re.search(r"(\d+\.\d+\.\d+)", output)
             if match:
                 gcc_version = match.group(1)
                 print(f"GCC version: {gcc_version}")
             if "8.1" < gcc_version:
-                print("GCC version is greater than or equal to 8.1.0, skip installation")
+                print(
+                    "GCC version is greater than or equal to 8.1.0, skip installation"
+                )
                 return None
         except Exception as e:
             print("gcc error: {}".format(e))
@@ -103,8 +104,12 @@ class Install:
         self._cmd("sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test")
         self._cmd("sudo apt update")
         self._cmd("sudo apt install -y gcc-9 g++-9")
-        self._cmd("sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 100")
-        self._cmd("sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100")
+        self._cmd(
+            "sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 100"
+        )
+        self._cmd(
+            "sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100"
+        )
         self._cmd("sudo ldconfig")
         self._cmd("gcc --version")
         self._cmd("g++ --version")
@@ -114,9 +119,14 @@ class Install:
         print("start to install cmake")
         cmake_version = "0.0.0"
         try:
-            result = subprocess.run(['cmake', '--version'], stdout=subprocess.PIPE, universal_newlines=True, check=True)
+            result = subprocess.run(
+                ["cmake", "--version"],
+                stdout=subprocess.PIPE,
+                universal_newlines=True,
+                check=True,
+            )
             output = result.stdout.splitlines()[0]
-            match = re.search(r'(\d+\.\d+\.\d+)', output)
+            match = re.search(r"(\d+\.\d+\.\d+)", output)
             if match:
                 cmake_version = match.group(1)
                 print(f"CMake version: {cmake_version}")
@@ -131,8 +141,17 @@ class Install:
             pass
         else:
             cmake_name = "cmake-3.22.0-linux-aarch64"
-        print("CMake version is less than 3.20, start installation ({})".format(cmake_name))
-        self._cmd("wget -t 10 {} -P {}".format("https://cmake.org/files/v3.22/{}.tar.gz".format(cmake_name), self._dowload_path))
+        print(
+            "CMake version is less than 3.20, start installation ({})".format(
+                cmake_name
+            )
+        )
+        self._cmd(
+            "wget -t 10 {} -P {}".format(
+                "https://cmake.org/files/v3.22/{}.tar.gz".format(cmake_name),
+                self._dowload_path,
+            )
+        )
         os.chdir(self._dowload_path)
         self._cmd("tar -zxvf {}.tar.gz".format(cmake_name))
         self._cmd("cp -r {}/bin/* /usr/local/bin".format(cmake_name))
@@ -146,15 +165,16 @@ class Install:
 
     def _clone_nlohmann_json(self):
         self._clone_github_repo(
-            "https://github.com/nlohmann/json.git",
-            "nlohmann_json",
-            "--depth=1"
+            "https://github.com/nlohmann/json.git", "nlohmann_json", "--depth=1"
         )
         os.chdir(os.path.join(self._dowload_path, "nlohmann_json"))
         self._cmd("mkdir -p build")
         os.chdir("build")
         self._cmd(
-            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(self._install_prefix))
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -164,13 +184,16 @@ class Install:
             "tinyxml2",
             "--single-branch",
             "--branch=8.0.0",
-            "--depth=1"
+            "--depth=1",
         )
         os.chdir(os.path.join(self._dowload_path, "tinyxml2"))
         self._cmd("mkdir -p build")
         os.chdir("build")
         self._cmd(
-            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(self._install_prefix))
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -180,7 +203,7 @@ class Install:
             "gperftools",
             "--single-branch",
             "--branch=gperftools-2.8",
-            "--depth=1"
+            "--depth=1",
         )
         os.chdir(os.path.join(self._dowload_path, "gperftools"))
         self._cmd("./autogen.sh || sleep 1 && ./autogen.sh")
@@ -200,10 +223,14 @@ class Install:
             "PROJ",
             "--single-branch",
             "--branch=7.1.0",
-            "--depth=1"
+            "--depth=1",
         )
         os.chdir(os.path.join(self._dowload_path, "PROJ"))
-        self._cmd("patch -p1 < {}".format(os.path.join(self._current_path,"scripts/PROJ-7.1.0.patch")))
+        self._cmd(
+            "patch -p1 < {}".format(
+                os.path.join(self._current_path, "scripts/PROJ-7.1.0.patch")
+            )
+        )
         self._cmd("mkdir -p build")
         os.chdir("build")
         self._cmd(
@@ -222,28 +249,28 @@ class Install:
             "gflags",
             "--single-branch",
             "--branch=v2.2.0",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/google/glog.git",
             "glog",
             "--single-branch",
             "--branch=v0.4.0",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/google/googletest.git",
             "googletest",
             "--single-branch",
             "--branch=release-1.10.0",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/protocolbuffers/protobuf.git",
             "protobuf",
             "--single-branch",
             "--branch=v3.14.0",
-            "--depth=1"
+            "--depth=1",
         )
 
         os.chdir(os.path.join(self._dowload_path, "gflags"))
@@ -262,7 +289,9 @@ class Install:
         os.chdir("build")
         self._cmd(
             "cmake -DCMAKE_CXX_FLAGS='-fPIC' -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix))
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -271,7 +300,9 @@ class Install:
         os.chdir("build")
         self._cmd(
             "cmake -DCMAKE_CXX_FLAGS='-fPIC -w' -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix))
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -281,7 +312,9 @@ class Install:
         os.chdir("build")
         self._cmd(
             "cmake -Dprotobuf_BUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX={} ..".format(
-                self._install_prefix))
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
@@ -312,8 +345,11 @@ class Install:
         if not os.path.exists(os.path.join(self._dowload_path, dds_name)):
             self._cmd(
                 "wget -t 10 {} -P {}".format(
-                    "https://apollo-system.cdn.bcebos.com/archive/6.0/{}".format(dds_name),
-                    self._dowload_path),
+                    "https://apollo-system.cdn.bcebos.com/archive/6.0/{}".format(
+                        dds_name
+                    ),
+                    self._dowload_path,
+                ),
             )
 
         os.chdir(self._dowload_path)
@@ -328,28 +364,28 @@ class Install:
             "asio",
             "--single-branch",
             "--branch=asio-1-18-1",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/eProsima/foonathan_memory_vendor.git",
             "foonathan_memory_vendor",
             "--single-branch",
             "--branch=v1.3.1",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/eProsima/Fast-CDR.git",
             "Fast-CDR",
             "--single-branch",
             "--branch=v2.2.2",
-            "--depth=1"
+            "--depth=1",
         )
         self._clone_github_repo(
             "https://github.com/eProsima/Fast-DDS.git",
             "Fast-DDS",
             "--single-branch",
             "--branch=v2.14.3",
-            "--depth=1"
+            "--depth=1",
         )
         os.chdir(os.path.join(self._dowload_path, "asio/asio"))
         self._cmd("./autogen.sh")
@@ -360,49 +396,84 @@ class Install:
         os.chdir(os.path.join(self._dowload_path, "foonathan_memory_vendor"))
         self._cmd("mkdir -p build")
         os.chdir("build")
-        self._cmd("cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix))
+        self._cmd(
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
         os.chdir(os.path.join(self._dowload_path, "Fast-CDR"))
-        self._cmd("patch -p1 < {}".format(os.path.join(self._current_path,"scripts/Fast-CDR_v2.2.2.patch")))
+        self._cmd(
+            "patch -p1 < {}".format(
+                os.path.join(self._current_path, "scripts/Fast-CDR_v2.2.2.patch")
+            )
+        )
         self._cmd("mkdir -p build")
         os.chdir("build")
-        self._cmd("cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix))
+        self._cmd(
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
         os.chdir(os.path.join(self._dowload_path, "Fast-DDS"))
         self._cmd("mkdir -p build")
         os.chdir("build")
-        self._cmd("cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
-                self._install_prefix))
+        self._cmd(
+            "cmake -DCMAKE_INSTALL_PREFIX={} -DBUILD_SHARED_LIBS=ON ..".format(
+                self._install_prefix
+            )
+        )
         self._cmd("make install -j$(nproc)")
         os.chdir(self._current_path)
 
         return None
 
     def _unpack_bvar(self):
-        download_url = "https://raw.githubusercontent.com/wiki/minhanghuang/CyberRT/libs"
+        download_url = (
+            "https://raw.githubusercontent.com/wiki/minhanghuang/CyberRT/libs"
+        )
         bvar_name = "bvar_9.0.0-rc-r2_amd64.deb"
         if "x86_64" == self._machine:
             pass
         else:
             bvar_name = "bvar_9.0.0-rc-r3_arm64.deb"
         download_url = download_url + "/" + bvar_name
-        self._cmd("wget -t 10 {} -O {}".format(download_url, os.path.join(self._dowload_path, "bvar.deb")))
-        self._cmd("dpkg -x {} {}".format(os.path.join(self._dowload_path, "bvar.deb"), os.path.join(self._dowload_path, "bvar")))
-        self._cmd("cp -r {}* {}".format(os.path.join(self._dowload_path, "bvar/usr/local/"), self._install_prefix))
+        self._cmd(
+            "wget -t 10 {} -O {}".format(
+                download_url, os.path.join(self._dowload_path, "bvar.deb")
+            )
+        )
+        self._cmd(
+            "dpkg -x {} {}".format(
+                os.path.join(self._dowload_path, "bvar.deb"),
+                os.path.join(self._dowload_path, "bvar"),
+            )
+        )
+        self._cmd(
+            "cp -r {}* {}".format(
+                os.path.join(self._dowload_path, "bvar/usr/local/"),
+                self._install_prefix,
+            )
+        )
         return None
+
 
 def parse_config():
     parser = argparse.ArgumentParser(description="install")
-    parser.add_argument("--platform", type=str, default=platform.machine(), help="platform")
-    parser.add_argument("--install_prefix", type=str, default="install", help="install prefix")
+    parser.add_argument(
+        "--platform", type=str, default=platform.machine(), help="platform"
+    )
+    parser.add_argument(
+        "--install_prefix", type=str, default="install", help="install prefix"
+    )
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     args = parse_config()
